06b968173bc62b70738f1519efb734172ec9abb8
khadra <67019030+khadra1@users.noreply.github.com>
2022-06-28 18:44:47 +0100

    data files added

---
+++ templates/map.js
@@ -0,0 +1,11 @@
+let myMap = L.map("map", {
+    center: [45.52, -122.67],
+    zoom: 13
+  });
+  
+  // Adding a tile layer (the background map image) to our map:
+  // We use the addTo() method to add objects to our map.
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+  
